b64ded30bb7e78ec5ee15c2f19699fd8660043dd
John <johnolshea@gmail.com>
2025-11-19 19:19:53 +0100

    feat: allow CORS for all origins on /api/auth/*

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { Hono } from 'hono';
 import { auth } from './lib/auth';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
+
+app.use(
+  '/api/auth/*',
+  cors({
+    origin: '*',
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 app
   .on(['POST', 'GET'], '/api/auth/**', (c) => auth.handler(c.req.raw))
