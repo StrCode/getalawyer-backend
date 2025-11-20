996a1f2ba261939c81cd6ac652b5ac0a30b676e5
vector <samcodebello@gmail.com>
2025-11-20 10:36:57 +0100

    fix: added types/node dependencies to test deployment on Vercel

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,8 @@ import { db } from '@/db/db';
 import { openAPI } from 'better-auth/plugins';
 
 export const auth = betterAuth({
+  //TODO:remove if not fixed
+  baseURL: "https://getalawyer-backend-production.up.railway.app",
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
