9bc74b3485b7f324a1cd0b7a7f90416e8c7f90d5
vector <samcodebello@gmail.com>
2025-11-20 09:20:18 +0100

    feat: added middleware to save context and user

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,25 @@ import { Hono } from 'hono';
 import { auth } from './lib/auth';
 import { cors } from 'hono/cors';
 
-const app = new Hono();
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null
+  }
+}>();
+
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    await next();
+    return;
+  }
+  c.set("user", session.user);
+  c.set("session", session.session);
+  await next();
+});
 
 app.use(
   '/api/auth/*',
