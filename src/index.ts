a9c3e1aee7ce3dbdb191a4adb2574727c8cf0bd3
vector <samcodebello@gmail.com>
2025-11-21 11:19:01 +0100

    chore: updated the app

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,33 @@ import { auth } from './lib/auth';
 import { cors } from 'hono/cors';
 import { logger } from "hono/logger";
 
-const app = new Hono();
+const app = new Hono<{
+  Variables: {
+    user: typeof auth.$Infer.Session.user | null;
+    session: typeof auth.$Infer.Session.session | null
+  }
+}>();
+
+app.use("*", async (c, next) => {
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    await next();
+    return;
+  }
+  c.set("user", session.user);
+  c.set("session", session.session);
+  await next();
+});
 
 app.use(logger());
-
-// app.use("*", async (c, next) => {
-//   const session = await auth.api.getSession({ headers: c.req.raw.headers });
-//   if (!session) {
-//     c.set("user", null);
-//     c.set("session", null);
-//     await next();
-//     return;
-//   }
-//   c.set("user", session.user);
-//   c.set("session", session.session);
-//   await next();
-// });
-
 
 app.use(
   "/*",
   cors({
     origin: [
+      'https://getalawyer-frontend.up.railway.app',
       'https://getalawyer-frontend.vercel.app',
       'http://localhost:3000',
     ],
