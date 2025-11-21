b19f0cb51cf315374218a42d3b29bea2bcd12a87
vector <samcodebello@gmail.com>
2025-11-21 11:31:43 +0100

    feat: updated the schema to add the auth

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,8 @@ import { auth } from './lib/auth';
 import { cors } from 'hono/cors';
 import { logger } from "hono/logger";
 
-const app = new Hono<{
-  Variables: {
-    user: typeof auth.$Infer.Session.user | null;
-    session: typeof auth.$Infer.Session.session | null
-  }
-}>();
-
-app.use("*", async (c, next) => {
-  const session = await auth.api.getSession({ headers: c.req.raw.headers });
-  if (!session) {
-    c.set("user", null);
-    c.set("session", null);
-    await next();
-    return;
-  }
-  c.set("user", session.user);
-  c.set("session", session.session);
-  await next();
-});
-
+const app = new Hono();
 app.use(logger());
-
 app.use(
   "/*",
   cors({
@@ -34,7 +14,7 @@ app.use(
       'http://localhost:3000',
     ],
     // origin: process.env.CORS_ORIGIN || "",
-    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowMethods: ["GET", "POST", "PUT", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
     exposeHeaders: ['Content-Length'],
     credentials: true,
